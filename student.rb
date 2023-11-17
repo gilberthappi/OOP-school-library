f12f8efdd2b0a610afa7836bfec7daff588dfa9f
gilberthappi <gdushimimana6@gmail.com>
2023-11-17 11:30:30 +0200

    Fix linters error

--- student.rb
+++ student.rb
@@ -1,14 +1,13 @@
 require_relative 'person'
 
-# class representing a [student]
+# Class representing a student
 class Student < Person
-    def initialize(name, age, 
-   classroom, parent_permission: true)
-   super(name, age, parent_permission: parent_permission)
-   @classroom = classroom
-    end
+  def initialize(name, age, classroom, parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,13 @@
 require_relative 'person'
 
-# class representing a [teacher]
+# Class representing a teacher
 class Teacher < Person
-    def initialize(name, age,
-        specialization, permission: true)
-        super(name, age, permission)
-        @specialization = specialization
-    end
+  def initialize(name, age, specialization, permission: true)
+    super(name, age, permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
